2ba24a0d12a61a2e93e86492d278f45705f333bf
dases <cluttering+git@gmail.com>
2014-08-27 08:57:42 +1200

    support "reqparser" on swagger.operation

--- flask_restful_swagger/swagger.py
+++ flask_restful_swagger/swagger.py
@@ -287,7 +287,7 @@ class SwaggerEndpoint(object):
       op = {
           'method': method,
           'parameters': path_arguments,
-          'nickname': 'nickname'
+          'nickname': resource.__name__ + method_impl.__name__
       }
       op['summary'], op['notes'] = _parse_doc(method_impl)
 
@@ -295,7 +295,13 @@ class SwaggerEndpoint(object):
         # This method was annotated with @swagger.operation
         decorators = method_impl.__dict__['__swagger_attr']
         for att_name, att_value in list(decorators.items()):
-          if isinstance(att_value, (str, int, list)):
+          if att_name == 'reqparser':
+            # ensure a unique name if we need to create a model
+            model_name = decorators.get('nickname', op['nickname']) + 'Params'
+            op['parameters'] = merge_reqparse_args(op['parameters'],
+                                                   att_value,
+                                                   model_name)
+          elif isinstance(att_value, (str, int, list)):
             if att_name == 'parameters':
               op['parameters'] = merge_parameter_list(
                 op['parameters'], att_value)
@@ -309,6 +315,43 @@ class SwaggerEndpoint(object):
     return operations
 
 
+def merge_reqparse_args(base, reqparser, model_name):
+  """Translate a flask-restful RequestParser into swagger params.
+  If any of the reqparser args are location=json (which is the default)
+  then a model (named model_name) will be created with all json args.
+  """
+  model = {'id': model_name, 'properties': {}, 'required': []}
+  register_model = False
+  params = []
+  for arg in reqparser.args:
+    if 'json' in arg.location:
+      register_model = True
+      if arg.required:
+        model['required'].append(arg.name)
+      model['properties'][arg.name] = reqparse_arg_to_swagger_param(arg)
+    else:
+      param = reqparse_arg_to_swagger_param(arg)
+      # note: "cookies" location not supported by swagger
+      if arg.location == 'args':
+        param['paramType'] = 'query'
+      elif arg.location == 'headers':
+        param['paramType'] = 'header'
+      elif arg.location == 'view_args':
+        param['paramType'] = 'path'
+      else:
+        param['paramType'] = arg.location
+      params.append(param)
+
+  if register_model:
+    registry['models'][model_name] = model
+    params.append({'name': 'body',
+                   'paramType': 'body',
+                   'dataType': model_name,
+                   'required': len(model['required']) > 0
+                   })
+  return merge_parameter_list(base, params)
+
+
 def merge_parameter_list(base, override):
   base = list(base)
   names = [x['name'] for x in base]
@@ -320,6 +363,17 @@ def merge_parameter_list(base, override):
     else:
       base.append(o)
   return base
+
+
+def reqparse_arg_to_swagger_param(arg):
+  param = deduce_swagger_type(arg.type)
+  param['name'] = arg.name
+  param['description'] = arg.help
+  param['defaultValue'] = arg.default
+  param['required'] = arg.required
+  if arg.action == 'append':
+    param['allowMultiple'] = True
+  return param
 
 
 class SwaggerRegistry(Resource):
@@ -469,6 +523,9 @@ def deduce_swagger_type_flat(python_type_or_object, nested_type=None):
                                          fields.FormattedString,
                                          fields.Url)):
         return 'string'
+    if predicate(python_type_or_object, (bool,
+                                         fields.Boolean)):
+        return 'boolean'
     if predicate(python_type_or_object, (int,
                                          fields.Integer)):
         return 'integer'
@@ -477,9 +534,6 @@ def deduce_swagger_type_flat(python_type_or_object, nested_type=None):
                                          fields.Arbitrary,
                                          fields.Fixed)):
         return 'number'
-    if predicate(python_type_or_object, (bool,
-                                         fields.Boolean)):
-        return 'boolean'
     if predicate(python_type_or_object, (fields.DateTime,)):
         return 'date-time'
 
@@ -513,10 +567,12 @@ def extract_path_arguments(path):
     if len(spl) == 1:
       return {'name': spl[0],
               'dataType': 'string',
-              'paramType': 'path'}
+              'paramType': 'path',
+              'required': True}
     else:
       return {'name': spl[1],
               'dataType': spl[0],
-              'paramType': 'path'}
+              'paramType': 'path',
+              'required': True}
 
   return list(map(split_arg, args))
